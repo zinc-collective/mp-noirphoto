a14a0f18008d88537f6fafd413de4dc9fbef1150
Sean Clark Hess <seanhess@gmail.com>
2016-05-06 15:33:02 -0600

    2.2 build 6 - markdown info text

--- NoirPhoto/InfoViewController.swift
+++ NoirPhoto/InfoViewController.swift
@@ -7,21 +7,48 @@
 //
 
 import UIKit
+import SwiftyMarkdown
 
-class InfoViewController: UIViewController {
+class InfoViewController: UIViewController, UITextViewDelegate {
 
-    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var textView: UITextView!
+    
+//    let linkColor = UIColor(red: 0.827, green: 0.392, blue: 0.2235, alpha: 1.0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let image = UIImage(named: "InfoText")!
-        let imageView = UIImageView(image: image)
-        let heightToWidth = image.size.height / image.size.width
-        let width = view.frame.size.width
-        imageView.frame = CGRect(x: 0, y: 0, width: width, height: width * heightToWidth)
-        self.scrollView.contentSize = imageView.frame.size
-        self.scrollView.addSubview(imageView)
+//        let image = UIImage(named: "InfoText")!
+//        let imageView = UIImageView(image: image)
+//        let heightToWidth = image.size.height / image.size.width
+//        let width = view.frame.size.width
+//        imageView.frame = CGRect(x: 0, y: 0, width: width, height: width * heightToWidth)
+//        self.scrollView.contentSize = imageView.frame.size
+//        self.scrollView.addSubview(imageView)
+        
+        textView.dataDetectorTypes = .All
+        textView.delegate = self
+        textView.contentInset = UIEdgeInsets(top: 50, left: 0, bottom: 20, right: 0)
+//        textView.linkTextAttributes = [NSForegroundColorAttributeName: redColor]
+        
+        if let text = loadMD("info-text") {
+            text.body.color = UIColor.whiteColor()
+//            text.link.color = UIColor.greenColor() //
+            
+            textView.attributedText = text.attributedString()
+        }
+    }
+    
+    
+    func loadMD(name:String) -> SwiftyMarkdown? {
+        if let url = NSBundle.mainBundle().URLForResource(name, withExtension: "md") {
+            return SwiftyMarkdown(url: url)
+        }
+        return nil
+    }
+    
+    func textView(textView: UITextView, shouldInteractWithURL URL: NSURL, inRange characterRange: NSRange) -> Bool {
+        return true
     }
 
     override func viewWillAppear(animated: Bool) {
