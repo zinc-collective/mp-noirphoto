02228a34d4a01b717098884c90387d91478d07f8
theinternauts <cricketw@WeAreTheInternauts.com>
2024-04-08 23:38:21 +0900

    convert deprecated API for interfaceOrientation to modern extension for future use in iOS15+

--- NoirPhoto/Extensions/UIScene+Ext.swift
+++ NoirPhoto/Extensions/UIScene+Ext.swift
@@ -9,17 +9,22 @@
 import Foundation
 
 // for iOS 13+
-// in the future this should come from a UIWindowScene.windows
+enum SceneExtError: Error {
+    case windowSceneNotFound
+}
+
 extension UIScene {
     static var interfaceOrientation: UIInterfaceOrientation? {
         let scenes = UIApplication.shared.connectedScenes
-        guard let windowScenes = scenes.first as? UIWindowScene else {
-            fatalError("Could not obtain UIInterfaceOrientation from a valid windowScene")
+        if let windowScenes = scenes.first as? UIWindowScene {
+            return windowScenes.interfaceOrientation
+        } else {
+            AppDelegate().getAppLogger().logError(SceneExtError.windowSceneNotFound)
+            assertionFailure("Could not obtain UIInterfaceOrientation from a valid windowScene")
             return nil
         }
-        return windowScenes.interfaceOrientation
     }
-    static var isPortrait: Bool {
-        return Self.interfaceOrientation?.isPortrait ?? false
+    static var isLandscape: Bool {
+        return Self.interfaceOrientation?.isLandscape ?? false
     }
 }
