96fdf6007cddebdb1385b794656f614d4130109b
theinternauts <cricketw@WeAreTheInternauts.com>
2024-09-06 00:47:57 +0900

    not needed

--- NoirPhoto/Uitls/Sharing/ShareService.swift
+++ NoirPhoto/Uitls/Sharing/ShareService.swift
@@ -27,7 +27,6 @@ protocol ShareableActivityProvider {
 
 final class ShareService: ShareableActivityProvider {
     private(set) weak var parent: UIViewController?
-    var shareCompletion: ShareableActivityProvider.ProviderCompletion?
     
     func shareItem(sender parent: UIViewController,
                    sourceRect: CGRect,
