d06068650ac2706314f5e75f30cca7d6a3fd617d
theinternauts <cricketw@WeAreTheInternauts.com>
2024-09-07 16:25:58 +0900

    move out to file

--- NoirPhoto/SceneDelegate.swift
+++ NoirPhoto/SceneDelegate.swift
@@ -25,25 +25,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
     }
 }
-
-
-// for iOS 13+
-enum SceneExtError: Error {
-    case windowSceneNotFound
-}
-
-extension UIScene {
-    static var interfaceOrientation: UIInterfaceOrientation? {
-        let scenes = UIApplication.shared.connectedScenes
-        if let windowScenes = scenes.first as? UIWindowScene {
-            return windowScenes.interfaceOrientation
-        } else {
-            AppDelegate().getAppLogger().logError(SceneExtError.windowSceneNotFound)
-            assertionFailure("Could not obtain UIInterfaceOrientation from a valid windowScene")
-            return nil
-        }
-    }
-    static var isLandscape: Bool {
-        return Self.interfaceOrientation?.isLandscape ?? false
-    }
-}
