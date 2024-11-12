4515b6d1efca76c6067524fd5c26abc498959744
theinternauts <cricketw@WeAreTheInternauts.com>
2024-11-13 00:42:51 +0900

    update deprecated methods

--- NoirPhoto/Legacy/Classes/CustomPickerView.m
+++ NoirPhoto/Legacy/Classes/CustomPickerView.m
@@ -63,7 +63,8 @@
 	_bOutSet = YES;
 
 	float visibleHeight = _scrollView.frame.size.height;
-	CGPoint contentOffset = _scrollView.contentOffset;
+    NSLog(@"## - setTheCurrentValue: %.2f", visibleHeight);
+    CGFloat duration = 0.3f;
 	float totalHeight = _useHeight;
 
 	float curUseValue = value - _minValue;
@@ -74,15 +75,11 @@
 
 
 	//move to new offset Y
-	[UIView beginAnimations:@"movement" context:nil];
-	[UIView setAnimationCurve:UIViewAnimationCurveLinear];
-	[UIView setAnimationDuration:0.3f];
-
-	contentOffset.y = offsetY;
-	_scrollView.contentOffset = contentOffset;
-
-	[UIView commitAnimations];
-
+    [[UIView class] animateWithDuration:duration delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
+        CGPoint contentOffset = self->_scrollView.contentOffset;
+        contentOffset.y = offsetY;
+        self->_scrollView.contentOffset = contentOffset;
+    } completion:nil];
 }
 
 -(void)setTheCurrentValue2:(float)value
